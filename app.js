0dbda48a867cdcc943e47d5bf469e907c81bfc57
Ericklevy <erickfga23@gmail.com>
2025-03-18 18:41:30 -0300

    adicionado o desafio da aula

---
+++ app.js
@@ -0,0 +1,42 @@
+arrayAmigos = [];
+
+function adicionarAmigo() {
+    let addAmigo = document.querySelector('input').value;
+
+    if (addAmigo.trim() === '') {
+        alert('Por favor, insira um nome válido.');
+        return;
+    }
+
+    arrayAmigos.push(addAmigo);
+    console.log(arrayAmigos);
+    document.querySelector('input').value = '';
+    atualizarListaAmigos();
+}
+
+function atualizarListaAmigos() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    arrayAmigos.forEach(amigo => {
+        let li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    let amigoSorteado = arrayAmigos[Math.floor(Math.random() * arrayAmigos.length)];
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = '';
+
+    let li = document.createElement('li');
+    li.textContent = `O amigo secreto sorteado é: ${amigoSorteado}`;
+    resultado.appendChild(li);
+
+    arrayAmigos = [];
+    atualizarListaAmigos();
+}
+
+
+
